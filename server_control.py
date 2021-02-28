a490dd6ac3d3dc3a52f0df91ecb829077734afab
tech189 <dlloyd189@gmail.com>
2021-02-28 01:29:49 +0000

    requirements for venv, --stats, --system_sleep

--- server_control.py
+++ server_control.py
@@ -1,4 +1,4 @@
-import zmq, time, subprocess, sys, socket, minestat, logging.handlers, os, secrets
+import zmq, time, subprocess, sys, socket, minestat, logging.handlers, os, secrets, ast
 from interruptingcow import timeout
 
 if not os.path.exists("logs"):
@@ -96,17 +96,17 @@ def system_stop():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            with timeout(2, exception=RuntimeError):
+            with timeout(1, exception=RuntimeError):
                 logger.info("Pinging the server to check if it's online...")
                 s.connect((secrets.LOCAL_IP, 5555))
                 logger.info("Ping successful.")
                 pass
             with timeout(5, exception=RuntimeError):
-                    logger.info("Attempting to connect to the control server...")
-                    zmq_socket.connect("tcp://" + secrets.LOCAL_IP + ":5555")
-                    logger.info("Connected. Sending \"stop\" command...")
-                    zmq_socket.send_string("stop")
-                    pass
+                logger.info("Attempting to connect to the control server...")
+                zmq_socket.connect("tcp://" + secrets.LOCAL_IP + ":5555")
+                logger.info("Connected. Sending \"stop\" command...")
+                zmq_socket.send_string("stop")
+                pass
             logger.info("Done. Waiting for response...")
             response = zmq_socket.recv_string()
             if response == "done":
@@ -148,35 +148,75 @@ def system_stop():
 
     zmq_socket.close()
     zmq_context.term()
+
+def system_sleep():
+    logger.info("Sleeping the server, please wait...")
+
+    zmq_context = zmq.Context()
+    zmq_socket = zmq_context.socket(zmq.REQ)
+    zmq_socket.setsockopt(zmq.LINGER, 0) # The default value is -1, which means wait until all messages have been sent before allowing termination. Set to 0 to discard unsent messages immediately, and any positive integer will be the number of milliseconds to keep trying to send before discard.
+
+    # use os.path thingy to add danny bot's install folder to these path checks:
+    # if os.path.exists("disable_stop.txt"):
+    #     await message.channel.send("Not turning the server off because tech189 has requested that it stays on")
+    # else:
+    #     if os.path.exists("last_start.txt"):
+    #         os.remove("last_start.txt")
     try:
-        with timeout(5, exception=RuntimeError):
-            logger.info("Attempting to connect to the control server...")
-            zmq_socket.connect("tcp://" + secrets.LOCAL_IP + ":5555")
-            logger.info("Connected. Sending \"stop\" command...")
-            zmq_socket.send_string("stop")
-            pass
-        with timeout(5, exception=RuntimeError):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            with timeout(1, exception=RuntimeError):
+                logger.info("Pinging the server to check if it's online...")
+                s.connect((secrets.LOCAL_IP, 5555))
+                logger.info("Ping successful.")
+                pass
+            with timeout(5, exception=RuntimeError):
+                logger.info("Attempting to connect to the control server...")
+                zmq_socket.connect("tcp://" + secrets.LOCAL_IP + ":5555")
+                logger.info("Connected. Sending \"sleep\" command...")
+                zmq_socket.send_string("sleep")
+                pass
             logger.info("Done. Waiting for response...")
             response = zmq_socket.recv_string()
-            pass
-        if response == "done":
-            logger.info("The server has been switched on ✅")
+            if response == "done":
+                logger.info("Received response. Waiting 20 seconds for server to sleep...")
+                time.sleep(20)
+                s_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                try:
+                    with timeout(1, exception=RuntimeError):
+                        logger.info("Pinging the server to check if it's online...")
+                        s_2.connect((secrets.LOCAL_IP, 5555))
+                        logger.info("Ping successful. The server could not be put to sleep")
+                        zmq_socket.close()
+                        zmq_context.term()
+                        return {"result": "failed"}
+                except RuntimeError:
+                    logger.info("No response within 1 second. Therefore server is off")
+                    logger.info("The server is asleep 😴")
+                    zmq_socket.close()
+                    zmq_context.term()
+                    return {"result": "done"}
+                s_2.close()
+            else:
+                logger.info("Received response: " + response)
+                logger.info("The server could not be put to sleep")
+                zmq_socket.close()
+                zmq_context.term()
+                return {"result": "failed"}
+        except RuntimeError:
+            logger.info("The server seems to be off already, you can turn it on with `!server start`")
             zmq_socket.close()
             zmq_context.term()
             return {"result": "done"}
-        else:
-            logger.info("Received response: " + response)
-            logger.info("The server was switched on but the Minecraft Server was not switched on")
-            zmq_socket.close()
-            zmq_context.term()
-            return {"result": "failed"}
+        s.close()
     except RuntimeError:
-        logger.info("No response...")
-        logger.info("The server could not be switched on")
+        logger.info("The server could not be put to sleep")
         zmq_socket.close()
         zmq_context.term()
         return {"result": "failed"}
 
+    zmq_socket.close()
+    zmq_context.term()
 
 
 def status():
@@ -212,15 +252,34 @@ def status():
             ms2 = 'Online ✅\nVersion %s - %s out of %s players\nLatency: %sms\nModpack: %s\nPort: 25566' % (ms.version, ms.current_players, ms.max_players, ms.latency, ms.motd.decode("utf-8"))
         except RuntimeError:
             ms2 = 'Offline'
-        return {"result": "online  ✅", "ms1": ms1, "ms2": ms2}
+        
+        zmq_context = zmq.Context()
+        zmq_socket = zmq_context.socket(zmq.REQ)
+        zmq_socket.setsockopt(zmq.LINGER, 0) # The default value is -1, which means wait until all messages have been sent before allowing termination. Set to 0 to discard unsent messages immediately, and any positive integer will be the number of milliseconds to keep trying to send before discard.
+
+        try:
+            with timeout(5, exception=RuntimeError):
+                logger.info("Attempting to connect to the control server...")
+                zmq_socket.connect("tcp://" + secrets.LOCAL_IP + ":5555")
+                logger.info("Connected. Sending \"stats\" command...")
+                zmq_socket.send_string("stats")
+                pass
+            logger.info("Done. Waiting for response...")
+            stats = ast.literal_eval(zmq_socket.recv_string())
+        except RuntimeError:
+            stats = {'cpu_temp': '???'}
+        return {"result": "Online  ✅", "ms1": ms1, "ms2": ms2, "stats": stats}
     except RuntimeError:
         logger.info("No response within 1 second. Therefore server is off")
-        return {"result": "offline", "ms1": 'Offline', "ms2": 'Offline'}
+        return {"result": "Offline", "ms1": 'Offline', "ms2": 'Offline', "stats": {'cpu_temp': '???'}}
     sock.close()
 
 if "--system_start" in sys.argv:
     logger.info("COMMAND: system_start")
     print(system_start())
+elif "--system_sleep" in sys.argv:
+    logger.info("COMMAND: system_sleep")
+    print(system_sleep())
 elif "--system_stop" in sys.argv:
     logger.info("COMMAND: system_stop")
     print(system_stop())
